ec86c410689ee2169c333ddb195561a6b8843bbf
jordles113 <jordan.t.romero@gmail.com>
2021-01-13 14:18:16 -0600

    creates Home.js and Header.js and wires them up to each other and App

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='heading-primary'>Overlander</h1>
+            <h3 className='header__subtext'>Go Anywhere</h3>
+        </div>
+    )
+}
+
+export default Header
