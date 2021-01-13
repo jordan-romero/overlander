13f895ad0912a90bc1bdf87f63b9546e2b75eec6
jordles113 <jordan.t.romero@gmail.com>
2021-01-13 15:17:18 -0600

    styles header and header main and sub

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,8 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header__group'>
-                <h1 className='heading-primary'>Overlander</h1>
+                <h1 className='header__main'>Overlander</h1>
                 <h3 className='header__subtext'>Go Anywhere</h3>
-            </div>
         </div>
     )
 }
