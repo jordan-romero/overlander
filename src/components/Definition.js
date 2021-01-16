d3f422ac5699e73aac01d6c3012507278814b68d
jordles113 <jordan.t.romero@gmail.com>
2021-01-16 14:52:17 -0600

    writes markup in jsx for definition component

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Definition = () => {
     return (
         <section className='definition'>
-            <h1></h1>
+            <h1 className='definition__title'>Overland</h1>
+            <blockquote className='definition__quote'>“self-reliant overland travel to remote destinations where the journey is the principal goal. Typically, but not exclusively, it is accomplished with mechanized off-road capable transport (from bicycles to trucks) where the principal form of lodging is camping”</blockquote>
         </section>
     )
 }
