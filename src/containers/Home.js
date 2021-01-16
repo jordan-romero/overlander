c8df798fde6f50ba716a3f2a47e26cd261eb7c1c
jordles113 <jordan.t.romero@gmail.com>
2021-01-16 14:53:00 -0600

    wires up definition component with home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Definition from '../components/Definition'
 
 const Home = () => {
     return (
         <div className='home'>
             <Header />
+            <Definition />
         </div>
     )
 }
