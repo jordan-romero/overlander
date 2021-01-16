350418248461b0449a898424153ee94498937ef9
jordles113 <jordan.t.romero@gmail.com>
2021-01-16 15:00:30 -0600

    fixes quote markup in definition

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -4,7 +4,7 @@ const Definition = () => {
     return (
         <section className='definition'>
             <h1 className='definition__title'>Overland</h1>
-            <blockquote className='definition__quote'>“self-reliant overland travel to remote destinations where the journey is the principal goal. Typically, but not exclusively, it is accomplished with mechanized off-road capable transport (from bicycles to trucks) where the principal form of lodging is camping”</blockquote>
+            <blockquote className='definition__quote'>&ldquo; self-reliant overland travel to remote destinations where the journey is the principal goal. Typically, but not exclusively, it is accomplished with mechanized off-road capable transport (from bicycles to trucks) where the principal form of lodging is camping&rdquo;</blockquote>
         </section>
     )
 }
