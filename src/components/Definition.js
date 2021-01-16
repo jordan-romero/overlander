d9a3ecaeaf440b554611da9dc62f6a987fab5bbb
jordles113 <jordan.t.romero@gmail.com>
2021-01-16 14:50:55 -0600

    creates definition component

---
+++ src/components/Definition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Definition = () => {
+    return (
+        <section className='definition'>
+            <h1></h1>
+        </section>
+    )
+}
+
+export default Definition
