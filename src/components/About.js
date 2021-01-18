faaf2f6f03542e26451edf99ff8341244a916a72
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:14:04 -0600

    updates about component to have proper naming and content

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
+        <section className='about'>
+            <img src='../imgs/about.jpg' alt='photo of Glenn Romero' className='about__curve'/>
+            <h1>Meet Glenn Romero</h1>
+            <p>Glenn is an Attorney and in 2015, he decided he wanted to go anywhere. 
+            Come along on the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
+            </p>
             
-        </div>
+        </section>
     )
 }
 
