2b32bec692ccdd08e7616017589aefc58679421b
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:20:27 -0600

    properly imports about photo

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import AboutImg from '../imgs/about.jpg'
 
 const About = () => {
     return (
         <section className='about'>
-            <img src='../imgs/about.jpg' alt='photo of Glenn Romero' className='about__curve'/>
+            <img src={AboutImg} alt='photo of Glenn Romero' className='about__curve'/>
             <h1>Meet Glenn Romero</h1>
             <p>Glenn is an Attorney and in 2015, he decided he wanted to go anywhere. 
             Come along on the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
