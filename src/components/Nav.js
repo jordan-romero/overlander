214300ac32694d1bde1bc9f25dc0fd31b58aa10f
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 19:00:50 -0600

    working on jsx for nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Truck from '../imgs/truckshot.jpeg'
 
 const Nav = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='nav'>
+            <Navlink className='nav__link' to="/reviews">
+                <h2>Gear Reviews</h2>
+                <p>The top place for outfitting your vehicles and campers</p>
+                <img src={Truck} alt='photo of truck'/>
+            </Navlink>
+        </nav>
     )
 }
 
