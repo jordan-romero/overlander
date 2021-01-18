08d99c052431a530c61f4fa0d5532a4ecc8a8362
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 19:05:25 -0600

    wiring up second nav button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,6 +12,11 @@ const Nav = () => {
                     <p>The top place for outfitting your vehicles and campers</p>
                     <img src={Truck} alt='photo of truck'/>
                 </NavLink>
+                <NavLink className='nav__link' to="/reviews">
+                    <h2>Tips and Tricks</h2>
+                    <p>Exclusive information & guides for travel and maintenance</p>
+                    <img src={Truck} alt='photo of truck'/>
+                </NavLink>
             </nav>
         </Router>
     )
