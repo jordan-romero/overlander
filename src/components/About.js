4c45589eaeba4a97c4659b2c6fb2cd1dcb975efd
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:42:40 -0600

    adds styling for emphasis text and styles about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className='about'>
             <img src={AboutImg} alt='photo of Glenn Romero' className='about__curve'/>
             <h1 className='heading-secondary'>Meet Glenn Romero</h1>
-            <p className='about__text'>Glenn is an Attorney from Tampa, Florida. Throughout his life, he has travelled all across this country and in 2015, he decided he wanted to go anywhere. 
+            <p className='about__text'>Glenn is an Attorney from Tampa, Florida. Throughout his life, he has travelled all across this country and in 2015, he decided he wanted to <span className='emphasis'>go anywhere</span>. 
             Come along on the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
             </p>
             
