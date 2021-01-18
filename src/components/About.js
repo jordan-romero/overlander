44c6d0ab853d3154f1f96f29b5fc806065b007d2
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:39:02 -0600

    plays with sizing on img for about section, updates classNames in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <section className='about'>
             <img src={AboutImg} alt='photo of Glenn Romero' className='about__curve'/>
-            <h1 className=''>Meet Glenn Romero</h1>
-            <p>Glenn is an Attorney and in 2015, he decided he wanted to go anywhere. 
+            <h1 className='heading-secondary'>Meet Glenn Romero</h1>
+            <p className='about__text'>Glenn is an Attorney from Tampa, Florida. Throughout his life, he has travelled all across this country and in 2015, he decided he wanted to go anywhere. 
             Come along on the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
             </p>
             
