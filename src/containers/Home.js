badd7e22fbf29ce66db1598513b8bd061dbc39b7
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:14:53 -0600

    wires up About component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Definition from '../components/Definition'
+import About from '../components/About'
 
 const Home = () => {
     return (
         <div className='home'>
             <Header />
             <Definition />
+            <About />
         </div>
     )
 }
