0e392e265be0da769ef3cc27a6514074fe04f176
jordles113 <jordan.t.romero@gmail.com>
2021-01-17 18:48:56 -0600

    finishes styling about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,13 @@ import AboutImg from '../imgs/about.jpg'
 const About = () => {
     return (
         <section className='about'>
-            <img src={AboutImg} alt='photo of Glenn Romero' className='about__curve'/>
-            <h1 className='heading-secondary'>Meet Glenn Romero</h1>
-            <p className='about__text'>Glenn is an Attorney from Tampa, Florida. Throughout his life, he has travelled all across this country and in 2015, he decided he wanted to <span className='emphasis'>go anywhere</span>. 
-            Come along on the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
-            </p>
-            
+            <div className='about__content'>
+                <img src={AboutImg} alt='photo of Glenn Romero' className='about__curve'/>
+                <h1 className='heading-secondary'>Meet Glenn Romero</h1>
+                <p className='about__text'>Glenn is an Attorney from Tampa, Florida. Throughout his life, he has travelled all across this country and in 2015, he decided he wanted to <span className='emphasis'>go anywhere</span>. 
+                Join the journey for tips, gear reviews and campsite recommendations. Follow him on crazy adventures all across the country. 
+                </p>
+            </div>
         </section>
     )
 }
