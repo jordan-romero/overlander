08eab4c6fb1659351784fa246138f0b10897ca40
jordles113 <jordan.t.romero@gmail.com>
2021-01-18 18:33:58 -0600

    creates sass partial for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,17 +12,17 @@ const Nav = () => {
                 <NavLink className='nav__link' to="/reviews">
                     <h2 className='heading-tertiary'>Gear Reviews</h2>
                     <p className='nav__text'>The top place for outfitting your vehicles and campers</p>
-                    <img src={Truck} alt='photo of truck' className='nav__img'/>
+                    <img src={Truck} alt='truck' className='nav__img'/>
                 </NavLink>
                 <NavLink className='nav__link' to="/tips">
                     <h2 className='heading-tertiary'>Tips and Tricks</h2>
                     <p className='nav__text'>Exclusive information & guides for travel and maintenance</p>
-                    <img src={Tips} alt='photo of glenn and dog' className='nav__img'/>
+                    <img src={Tips} alt='glenn and dog' className='nav__img'/>
                 </NavLink>
                 <NavLink className='nav__link' to="/recommendations">
                     <h2 className='heading-tertiary'>Camping Recs</h2>
                     <p className='nav__text'>Recommendations for interesting and unique places to overland</p>
-                    <img src={Camper} alt='photo of glenn and dog' className='nav__img'/>
+                    <img src={Camper} alt='camper' className='nav__img'/>
                 </NavLink>
             </nav>
         </Router>
