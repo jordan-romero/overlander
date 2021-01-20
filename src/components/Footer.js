5aa2fe415971e3dbcf8b688ae913e3619848bf1a
jordles113 <jordan.t.romero@gmail.com>
2021-01-19 19:14:06 -0600

    adds contact links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import sprite from '../imgs/sprite.svg'
 
 const Footer = () => {
     return (
         <section className='footer'>
-            <div className='footer__contact'>
-                
+            <div className='footer__links'>
+                <a href='https://www.linkedin.com/in/jordan-t-romero/'>
+                    <svg className="icon icon--footer">
+                        <use href={sprite + '#icon-linkedin'} />
+                    </svg>
+                </a>
+                <a href='https://github.com/jordles113'>
+                    <svg className="icon icon--footer">
+                        <use href={sprite + '#icon-github'} />
+                    </svg>
+                </a>
+                <a href='mailto: jordanromero1390@gmail.com'>
+                    <svg className="icon icon--footer">
+                        <use href={sprite + '#icon-mail'} />
+                    </svg>
+                </a>
             </div>
+    
             <div className='footer__subscribe'>
             
             </div>
