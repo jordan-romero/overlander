0716b508c4f098e3b8125da6794a7440aaac2308
jordles113 <jordan.t.romero@gmail.com>
2021-01-22 18:18:40 -0600

    adds react router transitions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Home from './containers/Home'
-
+import { BrowserRouter as Router, Route, Switch, useHistory} from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 function App() {
+  const history = useHistory();
   return (
-    <div className="App">
-        <Home />
-    </div>
+    <div className='App'>
+      <Router>
+      <Route render={({location}) => (
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={3000}
+            classNames='fade'
+           >
+          <Switch location={location}>
+           
+            <Route exact path="/" component={Home} history={history}/>
+          </Switch>
+          </CSSTransition>
+          </TransitionGroup>
+      )}/>
+      </Router>
+      </div>
   );
 }
 
