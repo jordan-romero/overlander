28edb88147517220720abb82a165a1607d44dbb7
jordles113 <jordan.t.romero@gmail.com>
2021-01-24 16:27:20 -0600

    playing with hard coded state"

--- src/containers/Reviews.js
+++ src/containers/Reviews.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const Reviews = () => {
+
+    state = {
+        blogs = [
+            blog1 = {
+                title = 'The Best Off-the-Beaten path drives in CO',
+                content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia ut turpis non facilisis. Aenean lobortis nibh ut purus euismod, tincidunt rutrum justo pulvinar. Etiam at ex sit amet est euismod euismod. Vivamus eu dui iaculis, pretium urna at, condimentum dui. Proin quis diam convallis, semper est id, tristique nunc. Phasellus elit neque, ornare vel massa vel, ullamcorper molestie metus. Nunc quis dapibus nisl. Sed porta imperdiet massa eu laoreet. Vestibulum dolor magna, vestibulum et tempor ac, aliquet et lorem. Etiam semper bibendum metus quis congue. Phasellus et orci non felis auctor pulvinar in sit amet nulla.'
+            }
+            ] 
+    }
     return (
         <div>
             Hola from reviews!
